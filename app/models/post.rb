e8800ad1f3d4e94dd13a894bf55e469fe3efedf0
Trace <tracedelange@me.com>
2021-09-09 09:16:20 -0500

    commit tree 31792c96bce1fe23d19dd7d267ea350c1bbc096f

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,24 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true 
+    validates :content, length: { minimum: 250 } 
+    validates :summary, length: { maximum: 250 } 
+    validates :category, inclusion: {in: %w(Non-Fiction Fiction)}
+
+    validate :clickbait_title
+
+    def clickbait_title
+        # if title.exclude?(["Won't Believe", "Secret", "Top", "Guess"])
+        #     errors.add(:title, "Title is Certainly not clickbait-y enough")
+        # end
+        
+        if ["Won't Believe", "Secret", "Top", "Guess"].any?(title)
+            puts "Nice title."
+        else
+            errors.add(:title, "Title is Certainly not clickbait-y enough")
+        end
+            
+    end
+
 end
+
